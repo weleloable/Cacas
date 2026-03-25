3839b0dfa1149c1d5792f77319e0e3196b18ffd0
weleloable <weleloable@gmail.com>
2026-03-25 21:56:43 +0100

    Mis cambios

--- Cacas.py
+++ Cacas.py
@@ -66,7 +66,7 @@ with st.sidebar:
     if st.session_state.user is not None:
         # Verificamos si el usuario es un objeto de Supabase o un string de la cookie
         if isinstance(st.session_state.user, str):
-            st.write(f"✅ Sesión recordada: {st.session_state.user.user_metadata.get('full_name', st.session_state.user.email)}")
+            st.write(f"✅ Sesión recordada: {st.session_state.user.email}")
         else:
             nombre_display = st.session_state.user.user_metadata.get('full_name', st.session_state.user.email)
             st.write(f"✅ Sesión activa: {nombre_display}")
