57d8b92f0e9044ebaa030be5e0afed70c9898939
weleloable <weleloable@gmail.com>
2026-03-25 21:02:19 +0100

    Mis cambios

--- Cacas.py
+++ Cacas.py
@@ -97,7 +97,7 @@ if st.session_state.user is None:
 
 # --- 3.2. LOGUEADO: DATOS DE USUARIO ---
 curr_user = st.session_state.user
-st.write(curr_user, st.session_state.user, res)
+st.write(res)
 USER_ID = curr_user.id
 USER_NAME = curr_user.user_metadata.get('full_name', curr_user.email)
 
