3b53d8a93abb9f486bcda37418f73427d9c98508
weleloable <weleloable@gmail.com>
2026-09-14 09:08:25 +0200

    Publicar Gotita (ff63c0e)

--- sw.js
+++ sw.js
@@ -26,8 +26,8 @@
 
 // Estas dos líneas las reescribe el build, buscándolas por su texto exacto.
 // Si no las encuentra, el build falla en vez de publicar un sw a medias.
-const VERSION = "abecc6309874";
-const DEL_ARRANQUE = ["./","./manifest.json","./iconos/icono-192.png","./iconos/icono-512.png","./_expo/static/js/web/entry-abecc630987401a900b9856c01aa402e.js"];
+const VERSION = "7e85b802e4e3";
+const DEL_ARRANQUE = ["./","./manifest.json","./iconos/icono-192.png","./iconos/icono-512.png","./_expo/static/js/web/entry-7e85b802e4e3eee399cb260b52fc3756.js"];
 
 const CACHE = `gotita-${VERSION}`;
 
